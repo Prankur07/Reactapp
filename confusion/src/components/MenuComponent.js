f67dde7d34971864403e1d7af1515b2d6290ba41
Prankur07 <prankursingrol07@gmail.com>
2020-06-28 19:59:02 +0530

    Assignment1

--- confusion/src/components/MenuComponent.js
+++ confusion/src/components/MenuComponent.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { Media } from 'reactstrap';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody,
     CardTitle } from 'reactstrap';
+import DishdetailComponent from "./DishdetailComponent";
 
 class Menu extends Component {
 
@@ -18,16 +18,7 @@ class Menu extends Component {
     }
 
     renderDish(dish) {
-        if (dish != null)
-            return(
-                <Card>
-                    <CardImg top src={dish.image} alt={dish.name} />
-                    <CardBody>
-                      <CardTitle>{dish.name}</CardTitle>
-                      <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
+        if (dish != null) return <DishdetailComponent dish={dish} />;
         else
             return(
                 <div></div>
@@ -54,11 +45,7 @@ class Menu extends Component {
                 <div className="row">
                     {menu}
                 </div>
-                <div className="row">
-                  <div  className="col-12 col-md-5 m-1">
-                    {this.renderDish(this.state.selectedDish)}
-                  </div>
-                </div>
+                {this.renderDish(this.state.selectedDish)}
             </div>
         );
     }
