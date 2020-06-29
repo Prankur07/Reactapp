cbca851ecf9c401b19dc08dfac3c443f0fe8e2ae
Prankur07 <prankursingrol07@gmail.com>
2020-06-29 14:39:57 +0530

    week 2 practice

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,26 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent';
-import { DISHES } from './shared/dishes';
+import { BrowserRouter } from "react-router-dom";
+import Main from "./components/MainComponent";
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES
-    };
-  }
+ 
+  
   render() {
     return (
+     
+      <BrowserRouter>
       <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes} />
+        <Main />
       </div>
+    </BrowserRouter>
     );
   }
 }
